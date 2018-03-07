e0c3372c857b0c561edf645367c73b16b34bc93b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-03-07 03:19:27 +0100

    prepare v1.0.1 release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def readme():
 
 setup(
   name='pyinvoke',
-  version='1.0.0',
+  version='1.0.1',
   license='MIT',
   long_description=readme(),
   url='https://github.com/NiklasRosenstein/py-invoke',
